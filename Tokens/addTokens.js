f979baadb4b254f6525134438f44c684e103b307
glizzy <73669084+glixzzy@users.noreply.github.com>
2021-05-14 16:24:20 -0500

    Add files via upload

---
+++ Tokens/addTokens.js
@@ -0,0 +1,15 @@
+var username = prompt('Enter your Blooket username');
+var amount = prompt('How many tokens do you want? (max 500 daily)');
+
+fetch("https://api.blooket.com/api/users/addtokens", {
+    method: "PUT",
+    "headers": {
+        "referrer": "https://www.blooket.com/",
+        "content-type": "application/json",
+        "authorization": localStorage.token
+    },
+    body: JSON.stringify({
+        addedTokens: amount,
+        name: username
+    })
+});
